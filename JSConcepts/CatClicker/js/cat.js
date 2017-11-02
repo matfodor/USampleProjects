1fb8096517a1e6b60124de8dd8b1c0412433de30
Máté Fodor <matfodor@gmail.com>
2017-11-02 20:21:43 +0100

    Added several cats

--- JSConcepts/CatClicker/js/cat.js
+++ JSConcepts/CatClicker/js/cat.js
@@ -1,13 +1,24 @@
-Cat = function (name, imageUrl) {
+Cat = function (id, name, imageUrl) {
+    this.id = id;
     this.name = name;
     this.imageUrl = imageUrl;
+    this.clickCount = 0;
 
-    this.renderHtml = function(){
-        var html = '';
-    
-        html += '<div>' + this.name + '</div>';
-        html += '<img src="' + this.imageUrl + '">';
-    
-        return html;
+    this.addClick = function(){
+        this.clickCount++;
+    }
+
+    this.renderCatHtmlInContainer = function(container) {
+        var $name = $('<div>' + this.name + '</div>');
+        var $clicksCount = $('<span>(' + this.clickCount + ')</span>');
+        var $img = $('<img height="100" width="100" src="' + this.imageUrl + '">');
+
+        var that = this;
+        $img.on('click', function(e){
+            that.addClick();
+            $clicksCount.html('<span>(' + that.clickCount + ')</span>');
+        });
+
+        $(container).append($name.append($clicksCount), $img);
     }
 }
--- JSConcepts/CatClicker/js/scripts.js
+++ JSConcepts/CatClicker/js/scripts.js
@@ -1,23 +1,13 @@
 (function () {
     var cats = [
-        new Cat('kiscica', 'https://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-30-57b30ad41bc90__605.jpg'),
-        new Cat('nagycica', 'https://i.pinimg.com/736x/21/95/18/21951812cad294dfa48b7eacada72f52--munchkin-kitten-cute-cats-photos.jpg')
+        new Cat(1, 'kiscica', 'https://static.boredpanda.com/blog/wp-content/uploads/2016/08/cute-kittens-30-57b30ad41bc90__605.jpg'),
+        new Cat(2, 'nagycica', 'https://i.pinimg.com/736x/21/95/18/21951812cad294dfa48b7eacada72f52--munchkin-kitten-cute-cats-photos.jpg'),
+        new Cat(3, 'nagycica', 'https://i.pinimg.com/736x/21/95/18/21951812cad294dfa48b7eacada72f52--munchkin-kitten-cute-cats-photos.jpg')
     ];
 
     $(function () {
-        var clickCounter = 0;
-
-        addCats();
-
-        $('img').on('click', function () {
-            $('#click-counter').text(++clickCounter);
+        $.each(cats, function (index, cat) {
+            cat.renderCatHtmlInContainer('#cat-container');
         });
     });
-
-    function addCats(){
-        $catsContainer = $('#cats-container');
-        $.each(cats, function(index, cat){
-            $catsContainer.append(cat.renderHtml());
-        });
-    }
 })();
